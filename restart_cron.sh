97af99557e20e608194521a4714a431336da78bf
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-06-25 06:57:31 -0500

    Use pgrep -q

--- restart_cron.sh
+++ restart_cron.sh
@@ -3,7 +3,7 @@
 cd /home/aaron/gowork/src/github.com/aaronriekenberg/pi-web
 
 if [ $(uname) = 'OpenBSD' ]; then
-  pgrep pi-web > /dev/null
+  pgrep -q pi-web
   if [ $? -eq 1 ]; then
     ./restart.sh > /dev/null 2>&1
   fi
