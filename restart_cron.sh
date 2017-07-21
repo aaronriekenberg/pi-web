b53d47bb0bedde1b54d23edae5f6b5e45498c394
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-07-21 07:13:39 -0500

    Make restart_cron.sh work on raspberrypi

--- restart_cron.sh
+++ restart_cron.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-cd /home/aaron/gowork/src/github.com/aaronriekenberg/pi-web
-
 if [ $(uname) = 'OpenBSD' ]; then
   pgrep -q pi-web
   if [ $? -eq 1 ]; then
+    cd /home/aaron/gowork/src/github.com/aaronriekenberg/pi-web
     ./restart.sh > /dev/null 2>&1
   fi
 else
-  echo 'not implemented'
-  exit 1
+  pgrep pi-web > /dev/null 2>&1
+  if [ $? -eq 1 ]; then
+    cd /home/pi/gowork/src/github.com/aaronriekenberg/pi-web
+    ./restart.sh > /dev/null 2>&1
+  fi
 fi
