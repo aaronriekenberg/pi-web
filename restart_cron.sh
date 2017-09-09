a03231c616434658f343a711198784cf6cd2b2a3
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-09-09 16:03:15 -0500

    Remove openbsd.

--- restart_cron.sh
+++ restart_cron.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-if [ $(uname) = 'OpenBSD' ]; then
-  pgrep -q pi-web
-  if [ $? -eq 1 ]; then
-    cd /home/aaron/gowork/src/github.com/aaronriekenberg/pi-web
-    ./restart.sh > /dev/null 2>&1
-  fi
-else
-  pgrep pi-web > /dev/null 2>&1
-  if [ $? -eq 1 ]; then
-    cd /home/pi/gowork/src/github.com/aaronriekenberg/pi-web
-    ./restart.sh > /dev/null 2>&1
-  fi
+pgrep pi-web > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+  cd /home/pi/gowork/src/github.com/aaronriekenberg/pi-web
+  ./restart.sh > /dev/null 2>&1
 fi
