4561713d7db1bed59aaccc22864f47946a874dff
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-03-21 19:32:53 -0500

    Better syntax.

--- pi-web.go
+++ pi-web.go
@@ -109,8 +109,7 @@ func buildMainPageString(configuration *Configuration, creationTime time.Time) s
 		Configuration: configuration,
 		LastModified:  formatTime(creationTime),
 	}
-	err := templates.ExecuteTemplate(&buffer, mainTemplateFile, mainPageMetadata)
-	if err != nil {
+	if err := templates.ExecuteTemplate(&buffer, mainTemplateFile, mainPageMetadata); err != nil {
 		logger.Fatalf("error executing main page template %v", err.Error())
 	}
 	return buffer.String()
@@ -180,8 +179,7 @@ func commandRunnerHandlerFunc(configuration *Configuration, commandInfo CommandI
 		}
 
 		var buffer bytes.Buffer
-		err = templates.ExecuteTemplate(&buffer, commandTemplateFile, commandRunData)
-		if err != nil {
+		if err = templates.ExecuteTemplate(&buffer, commandTemplateFile, commandRunData); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -273,8 +271,7 @@ func readConfiguration(configFile string) *Configuration {
 	}
 
 	var configuration Configuration
-	err = yaml.Unmarshal(source, &configuration)
-	if err != nil {
+	if err = yaml.Unmarshal(source, &configuration); err != nil {
 		logger.Fatalf("error parsing %v: %v", configFile, err.Error())
 	}
 
